e8393df11f1646b3616d8997690c4f6e14956144
Phuong Hoa <danielleee241@gmail.com>
2025-03-02 22:12:54 +0700

    gop signup vao login

--- 25sp-prj301-asm-g07/src/java/prj301asm/Controllers/LoginServlet.java
+++ 25sp-prj301-asm-g07/src/java/prj301asm/Controllers/LoginServlet.java
@@ -6,7 +6,10 @@
 package prj301asm.Controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,14 +21,15 @@ import prj301asm.User.UserDTO;
  *
  * @author hoade
  */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String action = request.getParameter("action");
-
         if (action == null || action.equals("login")) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
             UserDAO dao = new UserDAO();
             UserDTO user = dao.login(username, password);
             HttpSession session = request.getSession(true);
@@ -45,15 +49,65 @@ public class LoginServlet extends HttpServlet {
                 session.invalidate();
             }
             forwardToLoginJSP(request, response, "Logout Sucessfully");
+        } else if (action.equals("signup")) {
+            request.setAttribute("nextAction", "createUser");
+            request.getRequestDispatcher("signup.jsp").forward(request, response);
+        } else if (action.equals("createUser")) {
+            String name = request.getParameter("name");
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String passwordConfirm = request.getParameter("passwordConfirm");
+
+            if (!username.matches("^[a-zA-Z0-9]+$")) {
+                request.setAttribute("errorUsername", "Username cannot contain spaces or special characters.");
+                RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
+                rd.forward(request, response);
+                return;
+            }
+
+            if (password == null || passwordConfirm == null || !password.equals(passwordConfirm)) {
+                request.setAttribute("errorPassword", "Passwords do not match");
+                RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
+                rd.forward(request, response);
+                return;
+            }
+
+            UserDAO dao = new UserDAO();
+            UserDTO checkUser = dao.checkUsername(username);
+            if (checkUser != null) {
+                request.setAttribute("errorUsername", "Username already exists");
+                RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
+                rd.forward(request, response);
+                return;
+            }
+
+            boolean createAccount = dao.createAccount(username, password, name);
+            if (createAccount) {
+                request.setAttribute("success", "Account created successfully");
+            } else {
+                request.setAttribute("fail", "Account creation failed");
+            }
+
+            RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
+            rd.forward(request, response);
         }
     }
-    
+
     private void forwardToLoginJSP(HttpServletRequest request, HttpServletResponse response,
             String errorMessage) throws ServletException, IOException {
         request.setAttribute("error", errorMessage);
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
